59a37a2bf939a90429f7e616474e95c2e7110b88
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-22 17:33:14 -0700

    Adding html editing code related to ids in index.html within game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,16 @@ var guess = 9;
 var lettersGuessed = [];
 var wordList = ["goku", "vegeta", "frieza", "cell", "krillin"];
 
-// document.onkeyup = function(event) {
-//     var userInput = event.key;
+document.onkeyup = function(event) {
+    var userInput = event.key;
 
-//     document.getElementById("getStarted").textContent = "";
-//     document.getElementById("wins").textContent = "Wins: " + wins;
-//     document.getElementById("losses").textContent = "Losses " + losses;
-//     document.getElementById("spacing").textContent = "-----------------";
-//     document.getElementById("word").textContent = "Word: "; //Need to make a function that replaces all the letters with underscores. ;
-//     document.getElementById("numberOfGuesses").textContent = "# of Guesses: " + guess;
-//     document.getElementById("lettersGuessed").textContent = "Letters Guessed: "; // Need to make a function that adds all the current letters in array
+    document.getElementById("getStarted").textContent = "";
+    document.getElementById("wins").textContent = "Wins: " + wins;
+    document.getElementById("losses").textContent = "Losses " + losses;
+    document.getElementById("spacing").textContent = "-----------------";
+    document.getElementById("word").textContent = "Word: "; //Need to make a function that replaces all the letters with underscores.
+    document.getElementById("numberOfGuesses").textContent = "# of Guesses: " + guess;
+    document.getElementById("lettersGuessed").textContent = "Letters Guessed: "; // Need to make a function that adds all the current letters in lettersGuessed array and print them
 
 
-// }
+}
