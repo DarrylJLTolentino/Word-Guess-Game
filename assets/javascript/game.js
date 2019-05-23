26ecf37252341590d8ab10fac7c857fc469a7684
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-22 18:28:43 -0700

    Putting all document write functions and create word function in two functions: startUp() and reWrite() within game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var losses = 0;
 var guess = 9;
 var lettersGuessed = [];
 var wordList = ["goku", "vegeta", "frieza", "cell", "krillin"];
-var word;
+var joinedLettersGuessed;
 
 function createBlankWord() {
     var chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
@@ -14,17 +14,44 @@ function createBlankWord() {
     return chosenWord, blankWord;
 }
 
+function startUp() {
+    var chosenWord, blankWord = createBlankWord();
+    document.getElementById("getStarted").textContent = "";
+    document.getElementById("wins").textContent = "Wins: " + wins;
+    document.getElementById("losses").textContent = "Losses " + losses;
+    document.getElementById("spacing").textContent = "-----------------";
+    document.getElementById("word").textContent = "Word: " + blankWord;
+    document.getElementById("numberOfGuesses").textContent = "# of Guesses: " + guess;
+    document.getElementById("lettersGuessed").textContent = "Letters Guessed: ";
+}
+
+function reWrite() {
+    document.getElementById("getStarted").textContent = "";
+    document.getElementById("wins").textContent = "Wins: " + wins;
+    document.getElementById("losses").textContent = "Losses " + losses;
+    document.getElementById("spacing").textContent = "-----------------";
+    document.getElementById("word").textContent = "Word: " + blankWord;
+    document.getElementById("numberOfGuesses").textContent = "# of Guesses: " + guess;
+    document.getElementById("lettersGuessed").textContent = "Letters Guessed: " + joinedLettersGuessed;
+}
+
+startUp();
+
 document.onkeyup = function (event) {
     var userInput = event.key;
-    var chosenWord, blankWord = createBlankWord();
+
+    lettersGuessed.push(userInput);
+    
+    joinedLettersGuessed = lettersGuessed.join(",");
 
     document.getElementById("getStarted").textContent = "";
     document.getElementById("wins").textContent = "Wins: " + wins;
     document.getElementById("losses").textContent = "Losses " + losses;
     document.getElementById("spacing").textContent = "-----------------";
-    document.getElementById("word").textContent = "Word: " + blankWord; //Need to make a function that replaces all the letters with dashes.
+    document.getElementById("word").textContent = "Word: " + blankWord;
     document.getElementById("numberOfGuesses").textContent = "# of Guesses: " + guess;
-    document.getElementById("lettersGuessed").textContent = "Letters Guessed: "; // Need to make a function that adds all the current letters in lettersGuessed array and print them
+    document.getElementById("lettersGuessed").textContent = "Letters Guessed: " + joinedLettersGuessed;
+
 
 
 }
