2b039835daac581e2b9530ef615f87a76be5cec0
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-22 20:20:05 -0700

    Adding conditions to check for double characters and if user input is a letter within game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,40 +36,55 @@ var chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
 var blankWord = createBlankWord(chosenWord);
 startUp(blankWord);
 
+
 document.onkeyup = function (event) {
     var userInput = event.key;
-    lettersGuessed.push(userInput);
-    joinedLettersGuessed = lettersGuessed.join(",");
-    var counter = 0;
-    for (var i = 0; i < chosenWord.length; i++) {
-        if (userInput === chosenWord[i]) {
-            var newBlank = blankWord.split('');
-            newBlank[i] = chosenWord[i];
-            blankWord = newBlank.join('');
-        }
-        else {
-            counter++;
+    var thatCharAgain = false;
+    for (var i = 0; i < lettersGuessed.length; i++) {
+        if (userInput === lettersGuessed[i]) {
+            thatCharAgain = true;
         }
     }
-    if (counter === chosenWord.length) {
-        guess--;
-    }
-    if (blankWord === chosenWord){
-        wins++;
-        chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
-        blankWord = createBlankWord(chosenWord);
-        startUp(blankWord);
-        lettersGuessed = [];
-        joinedLettersGuessed = "";
-    }
-    if (guess === 0 && blankWord !== chosenWord) {
-        losses++;
-        chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
-        blankWord = createBlankWord(chosenWord);
-        startUp(blankWord);
-        lettersGuessed = [];
-        joinedLettersGuessed = "";
-    }
-    reWrite();
 
+    if (thatCharAgain !== true && event.keyCode >= 65 && event.keyCode <= 90) {
+        lettersGuessed.push(userInput);
+        joinedLettersGuessed = lettersGuessed.join(",");
+        var counter = 0;
+        for (var i = 0; i < chosenWord.length; i++) {
+            if (userInput === chosenWord[i]) {
+                var newBlank = blankWord.split('');
+                newBlank[i] = chosenWord[i];
+                blankWord = newBlank.join('');
+            }
+            else {
+                counter++;
+            }
+        }
+        if (counter === chosenWord.length) {
+            guess--;
+        }
+        if (blankWord === chosenWord) {
+            wins++;
+            chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+            blankWord = createBlankWord(chosenWord);
+            startUp(blankWord);
+            lettersGuessed = [];
+            joinedLettersGuessed = "";
+            guess = 9;
+        }
+        if (guess === 0 && blankWord !== chosenWord) {
+            losses++;
+            chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+            blankWord = createBlankWord(chosenWord);
+            startUp(blankWord);
+            lettersGuessed = [];
+            joinedLettersGuessed = "";
+            guess = 9;
+        }
+        reWrite();
+        thatCharAgain = false;
+    }
+    else {
+        thatCharAgain = false;
+    }
 }
